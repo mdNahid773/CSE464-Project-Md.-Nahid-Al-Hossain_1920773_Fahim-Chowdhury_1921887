e124ab3d81a8a885d28bbf7616b982899e5867a0
mdNahid773 <128293518+mdNahid773@users.noreply.github.com>
2023-12-18 23:02:19 +0600

    weather-app

---
+++ weather-app-final-project/lib/screens/city_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:weather_today_completed/screens/location_screen.dart';
+import 'package:weather_today_completed/services/weather.dart';
+import 'package:weather_today_completed/utils/constants.dart';
+
+class CityScreen extends StatefulWidget {
+  @override
+  _CityScreenState createState() => _CityScreenState();
+}
+
+class _CityScreenState extends State<CityScreen> {
+  late String cityName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/city_background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Image.asset(
+                      'images/ic_back.png',
+                      width: 32.0,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 24.0,
+                ),
+                child: TextField(
+                  style: TextStyle(
+                    fontFamily: 'Museo Moderno',
+                    color: Colors.black,
+                  ),
+                  decoration: kTextFieldInputDecoration,
+                  onChanged: (value) {
+                    cityName = value.toUpperCase();
+                  },
+                ),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: StadiumBorder(),
+                ),
+                onPressed: () async {
+                  var resp =
+                      await GetWeatherByLocation().getByLocation(cityName);
+                  //debugPrint(resp.toString());
+                  if (resp['name'].toString().toUpperCase() != cityName) {
+                    return;
+                  }
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return LocationScreen(
+                          locationWeather: resp,
+                        );
+                      },
+                    ),
+                  );
+                },
+                child: Text(
+                  'Search Weather',
+                  style: kButtonTextStyle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
